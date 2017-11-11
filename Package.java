52964ebe50a18982b37b7bacd41c735e1607f320
aaronchenac2 <32992686+aaronchenac2@users.noreply.github.com>
2017-11-11 12:31:47 -0800

    Add files via upload

---
+++ Package.java
@@ -0,0 +1,67 @@
+package pckg;
+
+import java.awt.Image;
+
+public abstract class Package
+{
+    private double x;
+
+    private double y;
+    
+    protected String name;
+
+    protected Image image;
+    
+    private int sleep = 0;
+    
+    public Package()
+    {
+    }
+    
+    public void setSleep( int s)
+    {
+        sleep = s;
+    }
+    
+    public int getSleep()
+    {
+        return sleep;
+    }
+    
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+    
+    public void setX(double newX)
+    {
+        x = newX;
+    }
+    
+    public void setY(double newY)
+    {
+        y = newY;
+    }
+    
+    public void setName(String n)
+    {
+        name = n;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    public Image getImage()
+    {
+        return image;
+    }
+
+    public abstract void execute();
+    
+}
